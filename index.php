24251d6f17a1885f9cc5d99187a9485eaa874c23
hermogene2001 <hermogene2001@gmail.com>
2025-11-05 11:07:08 +0200

    index page is created

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MIS - Home</title>
+</head>
+<body>
+    <h1>Welcome to MIS</h1>
+    <nav>
+        <a href="registration.php">Register</a> | 
+        <a href="login.php">Login</a>
+    </nav>
+</body>
+</html>
