5b5abb2c53afa87f0c4ede0214af838e84f276aa
hermogene2001 <hermogene2001@gmail.com>
2025-11-05 12:11:47 +0200

    25RP18177, the form is added successfully

--- src/register.php
+++ src/register.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Register - MIS</title>
-    <title>Register - MIS</title>
+    <title>Register - Student MIS</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="bootstrap/css/styles.css">
     <link rel="stylesheet" href="bootstrap/css/register.css">
